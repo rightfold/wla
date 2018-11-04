2c5b1a892ffc88452f72d663efba5028c5398d37
Foldr <rightfold+kloe@gmail.com>
2018-11-04 18:05:27 +0100

    Read config files from main

--- wla/main/wla.hs
+++ wla/main/wla.hs
@@ -4,15 +4,18 @@ module Main
 
 import Control.Monad.Free (foldFree)
 import Control.Monad.IO.Class (liftIO)
+import Data.Foldable (fold)
+import Data.Function ((&))
 import System.Environment (getArgs)
 
+import qualified Network.HTTP.Client as Http
 import qualified Network.HTTP.Client.TLS as Http.Tls
-import qualified Data.ByteString as Bs
 import qualified Network.Wai.Handler.Warp as Warp
 
 import Control.Logger (Logger (..))
-import Data.Secret (Secret (..))
-import Wla.Software.Zalando (Config (..), requestWishList)
+import Wla.Config (Config (..), Crawler (..), readConfig, readCrawlers)
+import Wla.Software.Zalando (requestWishList)
+import Wla.WishList (WishList)
 
 import qualified Wla.Crawl.Http as Crawl.Http
 import qualified Wla.Crawl.Log as Crawl.Log
@@ -22,16 +25,27 @@ import qualified Wla.Web as Web
 
 main :: IO ()
 main = do
-  tokenFile <- head <$> getArgs
-  token <- Secret <$> Bs.readFile tokenFile
+  arguments <- getArgs
+  (configPath, crawlersPath) <-
+    case arguments of { [a, b] -> pure (a, b)
+                      ; _      -> fail "Usage: wla CONFIG CRAWLERS" }
+
+  config <- readConfig configPath
+  crawlers <- readCrawlers crawlersPath
 
   http <- Http.Tls.newTlsManager
+  let crawlers' = fmap (materializeCrawler http) crawlers
 
-  let config = Config "zalando.nl" token
-  wishList <- Crawl.Http.runT http $
+  wishList <- fold <$> sequence crawlers'
+
+  -- FIXME: Listen on configHttpHost, not the Warp default.
+  Warp.run (configHttpPort config & fromIntegral) $
+    Web.application I18n.nlNl wishList
+
+materializeCrawler :: Http.Manager -> Crawler -> IO WishList
+materializeCrawler http (ZalandoCrawler config) =
+  Crawl.Http.runT http $
     let interpret = Crawl.Log.interpret (Logger (liftIO . print)) $
                       Crawl.UpstreamDyke.interpret $
                         Crawl.Http.interpret in
     foldFree interpret $ requestWishList config
-
-  Warp.run 8080 $ Web.application I18n.nlNl wishList
